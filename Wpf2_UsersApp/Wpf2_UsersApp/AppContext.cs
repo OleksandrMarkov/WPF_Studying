4ab4cd9d64be76b0a578e72a93efd445fc73bcc2
Oleksandr Markov <ol.markov1999@gmail.com>
2021-03-05 22:19:35 +0200

    +sqlite installation; + Users db; + classes User, AppContext

---
+++ Wpf2_UsersApp/Wpf2_UsersApp/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// + db
+using System.Data.Entity;
+
+namespace Wpf2_UsersApp
+{
+    class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppContext() : base("DefaultConnection") { }
+    }
+}
+
---
+++ Wpf2_UsersApp/Wpf2_UsersApp/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf2_UsersApp
+{
+    class User
+    {
+        private int id { get; set; }
+
+        private string login, password, email;
+
+        public User()
+        { }
+
+        public User(string login, string email, string password)
+        {
+            this.login = login;
+            this.password = password;
+            this.email = email;
+        }
+    }
+}
